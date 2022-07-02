3e8427b36f11bc62d53ce7eb3eacde6ba9c49c7e
idpan <idpanashari813@gmail.com>
2022-07-03 00:20:18 +0700

    --change-value py-1.2rem & px-2.5rem

--- src/utility/Button.js
+++ src/utility/Button.js
@@ -1,8 +1,10 @@
-export default function Button(props){
-    return(
-        <button onClick={props.onClick} 
-        className="py-[1.7rem] px-[2.9rem] bg-[#E08E4F] text-white font-medium font-montserrat " >
-            {props.children}
-        </button>
-    )
+export default function Button(props) {
+  return (
+    <button
+      onClick={props.onClick}
+      className={` py-[1.2rem] px-[2.5rem] bg-[#E08E4F] text-white font-medium font-montserrat capitalize ${props.className} `}
+    >
+      {props.children}
+    </button>
+  );
 }
