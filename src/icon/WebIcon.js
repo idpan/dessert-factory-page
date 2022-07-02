b22833d28f5338104e6cb42045e1edcb18cffcee
idpan <idpanashari813@gmail.com>
2022-07-02 21:57:13 +0700

    --add icon compnent

---
+++ src/icon/WebIcon.js
@@ -0,0 +1,10 @@
+export default function web_icon(props){
+    return(
+        <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M16.0001 29.3333C23.3639 29.3333 29.3334 23.3638 29.3334 16C29.3334 8.63619 23.3639 2.66666 16.0001 2.66666C8.63628 2.66666 2.66675 8.63619 2.66675 16C2.66675 23.3638 8.63628 29.3333 16.0001 29.3333Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M10.6668 16C10.6668 23.3638 13.0546 29.3333 16.0002 29.3333C18.9457 29.3333 21.3335 23.3638 21.3335 16C21.3335 8.63619 18.9457 2.66666 16.0002 2.66666C13.0546 2.66666 10.6668 8.63619 10.6668 16Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M2.66675 16H29.3334" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+
+    )
+}
