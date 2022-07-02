45cd821c5891dcda58648cf6743e47ab24c2daff
idpan <idpanashari813@gmail.com>
2022-07-02 22:01:34 +0700

    initial commit

---
+++ src/header/Header.js
@@ -0,0 +1,9 @@
+import Navbar from "./Navbar";
+
+export default function Header(props){
+    return(
+        <div>
+            <Navbar></Navbar>
+        </div>
+    )
+}
---
+++ src/header/Navbar.js
@@ -0,0 +1,11 @@
+import Logo from "../icon/Logo";
+
+export default function Navbar(props) {
+  return (
+    <nav>
+      <div>
+        <Logo></Logo>
+      </div>
+    </nav>
+  );
+}
