5161af8960c039e207229512b863db96c97b2317
idpan <idpanashari813@gmail.com>
2022-07-03 14:05:13 +0700

    initial cmmit

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,9 +1,11 @@
+import Hero from "./Hero";
 import Navbar from "./Navbar";
 
 export default function Header(props) {
   return (
     <header id="home">
       <Navbar></Navbar>
+      <Hero></Hero>
     </header>
   );
 }
--- src/header/Navbar.js
+++ src/header/Navbar.js
@@ -4,7 +4,7 @@ import Button from "../utility/Button";
 import Navlink from "./Navlink";
 export default function Navbar(props) {
   return (
-    <nav className=" bg-[#000000cc] h-[44px] w-100% md:h-[100px] ">
+    <nav className="fixed z-50 right-0 left-0 top-0 bg-[#000000cc] h-[44px] w-100% md:h-[100px] ">
       <div className="as-container h-full flex justify-between items-center ">
         <a href="./">
           <Logo className="h-[30px] w-[61px] md:w-[132px] md:h-[62px] "></Logo>
@@ -15,7 +15,7 @@ export default function Navbar(props) {
           <img src={toggle} alt="toggle" className="md:hidden" />
         </label>
         <div
-          className=" peer-checked:block hidden md:block
+          className=" peer-checked:block hidden md:block rounded-[3px]
         lmd:py-[30px] lmd:px-[20px] 
         lmd:bg-[#000000cc] lmd:absolute lmd:top-[67px] lmd:left-[20px] lmd:right-[20px]
         lmd:[&>a:not(:last-child)]:mb-[12px]  md:[&>a:not(:last-child)]:mr-[40px]
@@ -26,7 +26,7 @@ export default function Navbar(props) {
           <Navlink link="#contact">contact</Navlink>
           <Navlink link="#about">about</Navlink>
           <a href="./order">
-            <Button className="lmd:w-full lmd:text-[28px] lmd:py-[1rem] lmd:mt-[20px] md:py-[.8rem] md:px-[1.5rem] rounded-xl md:bg-[#E08E4Fcc] ">
+            <Button className="lmd:w-full lmd:text-[28px] lmd:py-[1rem] lmd:mt-[20px] md:py-[.8rem] md:px-[1.5rem] md:rounded-xl md:bg-[#E08E4Fcc] ">
               order now
             </Button>
           </a>
